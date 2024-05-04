f862859379da07a0df2c0d7cb5bad873df102e22
collajuan <collajuan@gmail.com>
2024-05-04 11:10:01 +0200

    nodejs

--- Bootcamp/Backend/2.3 NPM/index.js
+++
@@ -1,9 +0,0 @@
-//var generateName = require('sillyname');
-
-import generateName from "sillyname";
-var sillyName = generateName();
-
-// console.log(`My name is ${sillyName}.`);
-
-import superheroes from "superheroes";
-console.log(superheroes.random());
