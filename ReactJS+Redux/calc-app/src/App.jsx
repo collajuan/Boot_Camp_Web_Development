6c663232c9dd0ceeb699e653197ab2061561601f
collajuan <collajuan@gmail.com>
2024-06-20 13:37:43 +0200

    calc_app

--- ReactJS+Redux/calc-app/src/App.jsx
+++ ReactJS+Redux/calc-app/src/App.jsx
@@ -1,4 +1,6 @@
+// eslint no-eval:0
 import React, { useState } from "react";
+import words from "lodash.words"
 // import Button from "./components/Buttoon/Button";
 import Functions from "./components/Functions";
 import MathOperations from "./components/MathOperations";
@@ -11,10 +13,15 @@ import "./App.css";
 const App = () => {
   // Array Destructuring
   const [stack, setStack] = useState("");
+    //devuelve un array de stack per son los signos indicados
+  const items = words(stack, /[^-^+^*^/]+/g)
+//   condicional como un if
+  const value = items.length > 0 ? items[items.length-1]:"0";
 
   return (
     <main className="react-calculator">
-      <Result value={stack} />
+    {/* muestra unicamente el ultimo valor del array de numeros sin los signos */}
+      <Result value={value} />
       <Numbers
         onClickNumber={(number) => {
           console.log("Click en number", number);
@@ -22,13 +29,29 @@ const App = () => {
         }}
       />
       <Functions
-        onContentClear={() => console.log("Content Clear")}
-        onDelete={() => console.log("onDelete")}
+        onContentClear={() => {
+          console.log("Content Clear");
+          setStack("");
+        }}
+        onDelete={() => {
+          console.log("onDelete");
+          if (stack.length > 0) {
+            const newStack = stack.substring(0, stack.length - 1);
+            setStack(newStack);
+          }
+        }}
       />
       <section className="math-operations">
         <MathOperations
-          onClickOperation={(operation) => console.log("Operation:", operation)}
-          onClickEqual={(equal) => console.log("Equal:", equal)}
+          onClickOperation={(operation) => {
+            console.log("Operation:", operation);
+            setStack(`${stack}${operation}`);
+          }}
+          onClickEqual={(equal) => {
+            console.log("Equal:", equal);
+            //eval - hace funciones en js segun lo q se pasa
+            setStack(eval(stack.toString()));
+          }}
         />
       </section>
     </main>
