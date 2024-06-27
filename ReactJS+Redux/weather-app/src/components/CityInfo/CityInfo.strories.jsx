ad8e2dfea8898c150617a51b0aa32c4fda3eb730
collajuan <collajuan@gmail.com>
2024-06-27 12:10:52 +0200

    Storybook

---
+++ ReactJS+Redux/weather-app/src/components/CityInfo/CityInfo.strories.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default {
+    title: "CityInfo",
+    component: CityInfo
+}
+
+export const CityExample = () => (<CityInfo city={"Montevideo"} country={"Uruguay"}></CityInfo>)
