799a29710f3e756747dbd96657899c0bf847fe12
collajuan <collajuan@gmail.com>
2024-07-29 09:51:36 +0200

    dom

--- Bootcamp/JavaScript/Drum Kit Starting Files/index.js
+++ Bootcamp/JavaScript/Drum Kit Starting Files/index.js
@@ -50,5 +50,5 @@ function buttonAnimation (key) {
     document.querySelector("." + key).classList.add("pressed")
     setTimeout(function () {
         document.querySelector("." + key).classList.remove("pressed")
-    },100)
+    }, 100)
 }
