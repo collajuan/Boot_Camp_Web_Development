2df718e4a1efc331a8caa208fe05a43571a99aaf
collajuan <collajuan@gmail.com>
2024-08-15 12:53:14 +0200

    QR_image

--- Bootcamp/Backend/2.4 QR Code Project/index.js
+++ Bootcamp/Backend/2.4 QR Code Project/index.js
@@ -4,8 +4,18 @@
 3. Create a txt file to save the user input using the native fs node module.
 */
 import { input } from '@inquirer/prompts';
+import qr from 'qr-image'
+import fs from 'fs'
 
-const answer = await input({ message: 'Enter URL' });
-console.log(answer);
+const url = await input({ message: 'Enter URL' });
+console.log(url);
+
+var qr_svg = qr.image(url);
+qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+
+fs.writeFile('ULR.txt', url, (err) => {
+    if(err) throw err;
+    console.log('The file has been saved');    
+})
 
 
