2ebbb668da93db082e01a4e954697fb67e9cde2c
collajuan <collajuan@gmail.com>
2024-08-17 15:53:07 +0200

    Body_Parser

--- Bootcamp/Backend/3.4 Middleware/solution4.js
+++ Bootcamp/Backend/3.4 Middleware/solution4.js
@@ -1,26 +1,31 @@
 import express from "express";
 import bodyParser from "body-parser";
-// Las siguientes tres lineas resuelven la ubicacion relativa de los archivos
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var bandName = "";
 
-// Le da propiedad Body el request para poder utilizar las etiquetas y sus valores
-// La función app.use() en Express.js agrega middleware al proceso de procesamiento de solicitudes de la aplicación.
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function bandNameGenerator(req, res, next) {
+  console.log(req.body);
+  bandName = req.body["street"] + req.body["pet"];
+  next();
+}
+
+app.use(bandNameGenerator);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/submit", (req, res) => {
-  console.log(req.body);
-  
-  console.log(req.body.street + " " + req.body.pet);   
-})
+  res.send(`<h1>Your band name is:</h1><h2>${bandName}✌️</h2>`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
