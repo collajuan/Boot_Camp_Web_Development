336f7277c21ca11bc6758bdc4219b5722079cb2b
collajuan <collajuan@gmail.com>
2024-08-17 16:18:42 +0200

    CustomMiddleware

--- Bootcamp/Backend/3.4 Middleware/index3.js
+++ Bootcamp/Backend/3.4 Middleware/index3.js
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+const logger = (req, res, next) => {
+  console.log(`Metodo: ${req.method} y URL: ${req.url}`);
+  //! SIEMPRE DEBE IR AL FINAL DEL MIDDLEWARE
+  next() 
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
