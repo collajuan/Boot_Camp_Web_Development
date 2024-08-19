e57ac6ea86a3fa7932e99d56f12cf0eb52c6c20f
collajuan <collajuan@gmail.com>
2024-08-19 10:41:43 +0200

    SecretURL

--- Bootcamp/Backend/3.5 Secrets Project/index.js
+++ Bootcamp/Backend/3.5 Secrets Project/index.js
@@ -1,41 +1,32 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
-import express from "express";
-import bodyparser from "body-parser";
-
+import express from 'express'
+import bodyParser from 'body-parser'
+// Las siguientes tres lineas resuelven la ubicacion relativa de los archivos
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
+const password = 'ILoveProgramming'
 
-var passIsOk = false;
-
-app.use(bodyparser.urlencoded({extended: true}));
-
-//Middleware que verifica contraseña
-function passCheck(req, res, next) {
-    const password = req.body["password"];
-    console.log(password);
-    if (password === "JuanColla"){
-        passIsOk = true;
-    }
-    next();
-}
-app.use(passCheck);
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
-});
+  });
 
-app.post("/check", (req, res) => {
-  if (passIsOk) {
+
+app.post("/check" , (req, res) => {
+    console.log(req.body);
+    if(req.body.password === password) {
         res.sendFile(__dirname + "/public/secret.html");
     } else {
-        res.redirect("/");
-}});
+       res.redirect("back") 
+    }
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
