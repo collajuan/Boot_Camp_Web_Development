a1a9f8f8195e42df2fed1af07d6f7f726cf50e2f
collajuan <collajuan@gmail.com>
2024-08-22 20:08:41 +0200

    DynamicStyle

--- Bootcamp/React/my-react-app/src/main.jsx
+++ Bootcamp/React/my-react-app/src/main.jsx
@@ -4,17 +4,19 @@ import { createRoot } from 'react-dom/client'
 const date = new Date()
 const hour = date.getHours()
 let h1 = ''
-let color
+let color = {
+  color: ""
+}
 
-if (0 <= hour & hour < 12){
+if (hour < 12){
   h1='Good morning'
-  color = {color: 'red'}
-} else if (hour >= 12 & hour < 18){
+  color.color = 'red'
+} else if (hour < 18){
   h1='Good Afternoon'
-  color = {color: 'green'}
+  color.color = 'green'
 } else {
   h1='Good evening'
-  color = {color: 'blue'}
+  color.color =  'blue'
 }
 
 createRoot(document.getElementById('root')).render(
