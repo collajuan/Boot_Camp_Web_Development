08af196bc38e4b0ec065b1d89d38714460c3bbdd
collajuan <collajuan@gmail.com>
2024-10-29 15:23:01 +0100

    update

--- ReactJS+Redux/calc-app/src/index.jsx
+++
@@ -1,7 +0,0 @@
-// import ReactDOM from 'react-dom'; QUEDE SIN USO CON CreateRoot**********
-import { createRoot } from 'react-dom/client';
-import React from 'react';
-import App from './App';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
--- ReactJS+Redux/weather-app/src/components/CityInfo/CityInfo.strories.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default {
-    title: "CityInfo",
-    component: CityInfo
-}
-
-export const CityExample = () => (<CityInfo city={"Montevideo"} country={"Uruguay"}></CityInfo>)
